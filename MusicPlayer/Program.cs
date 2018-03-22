95b5a725aa48c594e47b5b611fbfe31a24949145
Patryk Bielecki <p.patrykbielecki@gmail.com>
2018-03-22 18:02:18 +0100

    Dispose example

---
+++ MusicPlayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace MusicPlayer
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(@"Please specify mp3 file location (e.g C:\\Users\UserName\Desktop\Music.mp3)...");
+            var path = Console.ReadLine();
+
+            var player = new WaveOutEvent();
+            var audioFile = new AudioFileReader(path);
+            player.Init(audioFile);
+            player.Play();
+
+            Console.WriteLine("Click any button to dispose and stop music");
+            Console.ReadKey();
+
+            player.Dispose();
+            audioFile.Dispose();
+
+            Console.WriteLine("Silence...");
+            Console.ReadKey();
+        }
+    }
+}
