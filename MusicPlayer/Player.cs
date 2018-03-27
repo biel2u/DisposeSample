1a93c016189927d4a15bf5fd634f8d99b9560b7b
Patryk Bielecki <p.patrykbielecki@gmail.com>
2018-03-27 18:25:52 +0200

    idispose

---
+++ MusicPlayer/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace MusicPlayer
+{
+    internal class Player : IDisposable
+    {
+        private readonly WaveOutEvent _waveOutEvent;
+        private AudioFileReader _audioFileReader;
+        private bool _disposed;
+
+        public Player()
+        {
+            _waveOutEvent = new WaveOutEvent();
+        }
+
+        public void GetPath(string path)
+        {                       
+            _audioFileReader = new AudioFileReader(path);
+            _waveOutEvent.Init(_audioFileReader);
+        }
+
+        public void Play()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException("Player");
+            }
+            _waveOutEvent.Play();
+        }
+
+        public void Pause()
+        {
+            _waveOutEvent.Stop();
+        }
+
+        public void Dispose()
+        {
+            _audioFileReader.Dispose();
+            _waveOutEvent.Dispose();
+            _disposed = true;
+        }
+    }
+}
--- MusicPlayer/Program.cs
+++ MusicPlayer/Program.cs
@@ -7,21 +7,37 @@ namespace MusicPlayer
     {
         private static void Main()
         {
+            var player = new Player();
+
             Console.WriteLine(@"Please specify mp3 file location (e.g C:\\Users\UserName\Desktop\Music.mp3)...");
             var path = Console.ReadLine();
 
-            var player = new WaveOutEvent();
-            var audioFile = new AudioFileReader(path);
-            player.Init(audioFile);
+            player.GetPath(path);
             player.Play();
 
-            Console.WriteLine("Click any button to dispose and stop music");
-            Console.ReadKey();
+            Console.WriteLine("Type 1 to PLAY, 2 to PAUSE, 3 to DISPOSE, 4 to EXIT");
+            var playerOption = Console.ReadLine();
+            do
+            {
+                if (playerOption == "1")
+                {
+                    player.Play();
+                }
+                else if (playerOption == "2")
+                {
+                    player.Pause();
+                }
+                else if (playerOption == "3")
+                {
+                    player.Dispose();
+                }
+                else
+                {
+                    Console.WriteLine("Option not recognized.");
+                }
+                playerOption = Console.ReadLine();
+            } while (playerOption != "4");
 
-            player.Dispose();
-            audioFile.Dispose();
-
-            Console.WriteLine("Silence...");
             Console.ReadKey();
         }
     }
